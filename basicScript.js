5bfcfd87166f19eede71650d188bea6d110d77d0
Serhii Kambur <sergey.2000.kambur@gmail.com>
2020-08-09 21:35:21 +0300

    Initial commit

---
+++ basicScript.js
@@ -0,0 +1,8 @@
+const firstName = 'Sergey'
+const age = 20
+const isProgrammer = true
+
+//console.log('Имя человека: ' + firstName + ' а возраст человека:' + age.toString())
+//console.log(age)
+const lastName = prompt('Введите фамилию')
+alert(firstName + ' ' + lastName)
